50e537bc4b2b416a40fcb2fdbf18051a244fb747
Bronson Valiquet Myers <owd.bob@gmail.com>
2019-08-15 19:43:49 +0000

    Fixed migrations, added minimagick gem for resizing

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   #storage :file
@@ -27,6 +27,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
+  process resize_to_fill: [800, 350]
 
   # Create different versions of your uploaded files:
   # version :thumb do
