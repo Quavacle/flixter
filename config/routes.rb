2cd38218762f3acb6e69ea66832168ded7dd0ab3
Bronson Valiquet Myers <owd.bob@gmail.com>
2019-08-15 00:56:56 +0000

    Added lesson detail, cleaned up section view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
+
   root 'static_pages#index'
   resources :courses, only: [:index, :show]
+  resources :lessons, only: [:show]
   namespace :instructor do
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
     end
