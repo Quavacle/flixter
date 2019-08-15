8145191a0300bcf38e5f5f1cb0033c9a9f4cb4c5
Bronson Valiquet Myers <owd.bob@gmail.com>
2019-08-15 20:05:39 +0000

    updated carrierwave to hopefully work with heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,13 @@
 # config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
-  config.storage = 'aws'                        # required
+ config.storage    = :aws
   config.aws_bucket = ENV["AWS_BUCKET"]
-  config.aws_acl = "public-read"
-  
-  config.aws_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
-    region:  ENV["AWS_DEFAULT_REGION"]
-  }
+  config.aws_acl    = "public-read"
 
+  config.aws_credentials = {
+      access_key_id:     ENV["AWS_ACCESS_KEY"],
+      secret_access_key: ENV["AWS_SECRET_KEY"],
+      region:            ENV["AWS_REGION"]
+  }
 end
