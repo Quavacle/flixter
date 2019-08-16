9b631602472f0624c6e1ab5640a86362114e701e
Bronson Valiquet Myers <owd.bob@gmail.com>
2019-08-16 19:33:59 +0000

    Added enrollment, basic logic and validation, minor aesthetic changes on courses/show

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   resources :users
 
   root 'static_pages#index'
-  resources :courses, only: [:index, :show]
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: :create
+  end
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :sections, only: [] do
